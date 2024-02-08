7e4936dbcb8ca8aa4549a531b7eb1227b06e1160
xwstx27 <xwstx.27@gmail.com>
2024-02-08 12:33:55 +0300

    New project on Spring Fraemwork with Lombok

---
+++ src/main/java/org/duck/duckbackend/DuckBackendApplication.java
@@ -0,0 +1,13 @@
+package org.duck.duckbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuckBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/duck/duckbackend/DuckBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.duck.duckbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
